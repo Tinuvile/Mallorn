cfe40fd5dd357938d2b2ded3391ce5264599f769
Tinuvile <143079146+Tinuvile@users.noreply.github.com>
2025-09-13 07:23:47 +0800

    update sql

--- Database/campus_trade_test_data.sql
+++ Database/campus_trade_test_data.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc7a83b29b5d2c1ff1376474750dd0e4143d315ef88132f71bd077ca2f097921
-size 236272395
+oid sha256:afbba7c0485519d3b3f2fe3a9a6c2abcc9b779392e578307b1496edf8c164654
+size 236272484
